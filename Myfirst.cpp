a450c13519ca079eabc16abf68c30ecc003f754a
lizi123321123 <you@example.com>
2019-03-23 14:37:12 +0800

    first commit

---
+++ Myfirst.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "hello.h"
+int main()
+{
+  hello thehello;
+  thehello.Display();
+ return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "hello.h"
+using namespace std;
+hello::hello()
+{
+}
+int hello::Display()
+{
+   cout<<"hello,ivy!\n"<<endl;
+return 0;
+}
